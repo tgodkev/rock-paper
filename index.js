182743a659eab6d66d50eaab96ef6c372ca26ecb
kevin knight <tgodkev@gmail.com>
2021-10-29 22:41:08 -0400

    play round cleaned up

--- index.js
+++ index.js
@@ -48,42 +48,22 @@ function computerPlay(){
 
 
 
-function playRound(playerSelection, computerSelection){
-    if( playerSelection === "rock" && computerSelection === "paper"){
-        computerScore++;
-        document.getElementById('cScore').append(computerScore++)
-        return "Player loses because paper covers rock";
-    }else if(playerSelection === "rock" && computerSelection === "scissors"){
-        playerScore++;
-        document.getElementById('pScore').append(playerScore++);
-        return "player wins" + " because rock crushes scissors"
-    }else if (playerSelection === "rock" && computerSelection === "rock"){
-        return "tie rock cant break rock";
-    } else if (playerSelection === "paper" && computerSelection === "rock"){
-        playerScore++;
-        document.getElementById('pScore').append(playerScore++);
-        return "player wins" + "because paper smothers rock"
-    } else if (playerSelection === "paper" && computerSelection === "paper"){
-        return "tie paper cant harm paper"
-     } else if (playerSelection === "paper" && computerSelection === "scissors"){
-         computerScore++;
-         document.getElementById('cScore').append(computerScore++);
-         return "computer wins scissors eat paper"
-     }
-     
-    if( playerSelection === "scissors" && computerSelection === "paper"){
-        playerScore++;
-        document.getElementById('pScore').append(playerScore++);
-        return "Player wins because scissors eat paper";
-    }else if(playerSelection === "scissors" && computerSelection === "scissors"){
-        return "tie try again pls"
-    } else if (playerSelection === "scissors" && computerSelection === "rock"){
-        computerScore++;
-        document.getElementById('cScore').append(computerScore++);
-        return "player loses becuse rock smash scissors";
+
+    function playRound(playerSelection, computerSelection){
+        if( playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper"){
+            playerScore++;
+            document.getElementById('pScore').append(playerScore++);
+        }
+        
+        else if(playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock"){
+            computerScore++;
+            document.getElementById('cScore').append(computerScore++)
+        }else{
+            draws++;
+            document.getElementById("tie").append(draws++);
+        }
     }
-    
-}
+
  
 
 
